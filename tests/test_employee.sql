0062e0b5f42369a34a329e81b9f9fbb09954e453
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-10-31 09:45:05 +0000

    Write multiple test cases as part of one model.

--- tests/test_employee.sql
+++ tests/test_employee.sql
@@ -2,9 +2,12 @@ select * from {{ ref('dim_employee')}}
 --where 1=1 -- always true
 where department_id >999
 
+union all
 
 select * from {{ ref('dim_employee')}}
 --where 1=1 -- always true
 where EMAIL not like '%com'
+
+
 
 
