33b3580c0778fde864ef473ffcbce16d3fd868dc
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-10-31 10:12:30 +0000

    Create a file in analyses file and copy test 2 sql for instance as if still building the query and not completed.

--- analyses/analyses_dim_employee.sql
+++ analyses/analyses_dim_employee.sql
@@ -8,8 +8,3 @@ select count(*) from {{ ref('dim_employee')}}
 --where 1=1 -- always true
 where EMAIL not like '%com'
 
-
-
-
-
-
--- tests/test2_employee.sql
+++ tests/test2_employee.sql
@@ -1,3 +1,11 @@
-select * from {{ ref('dim_employee')}}
+select 'test1',count(*) from {{ ref('dim_employee')}}
 --where 1=1 -- always true
 where EMAIL not like '%com'
+group by 1 
+having count(*)> 0
+
+
+
+
+
+
