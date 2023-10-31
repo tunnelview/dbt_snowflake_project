9fe17355f77d578943fad460889be0e284db8db3
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-10-31 09:28:11 +0000

    Write a custom test case for EMAIL column.

--- tests/test_employee.sql
+++ tests/test_employee.sql
@@ -1,2 +1,10 @@
 select * from {{ ref('dim_employee')}}
-where 1=1
+--where 1=1 -- always true
+where department_id >999
+
+
+select * from {{ ref('dim_employee')}}
+--where 1=1 -- always true
+where EMAIL not like '%com'
+
+
