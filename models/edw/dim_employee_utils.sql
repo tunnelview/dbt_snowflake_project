7a5b56a183c30e3b34d288cbdf0edf5195c6ada3
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-11-01 10:17:28 +0000

    Generate surrogate key using dbt_utils(3rd party package).

---
+++ models/edw/dim_employee_utils.sql
@@ -0,0 +1,13 @@
+{{ dbt_utils.generate_surrogate_key(['EMPLOYEE_ID']) }},
+EMPLOYEE_ID ,
+EMPLOYEE_NAME ,
+DEPARTMENT_ID ,
+EMAIL ,
+PHONE ,
+ADDRESS ,
+HIRE_DATE ,
+EMPLOYMENT_STATUS,
+MD5_COLUMN,
+current_timestamp as SNOW_INSERT_TIME,
+current_timestamp as SNOW_UPDATE_TIME
+FROM {{ ref('stg_dim_employee')}}
