e091698dbecbce9759454299aeaf1981887ddb21
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-11-06 09:55:25 +0000

    Rewrite the same code written in test_jinja_only_sql

---
+++ models/edw/test_jinja.sql
@@ -0,0 +1,21 @@
+{%- set dept_names = ['hr','sales','others'] -%}
+
+with dept as (
+select * from dbt_dev.src.seed_dept
+),
+final as (
+select
+dept_id,
+{% for dept_nm in dept_names -%}
+
+sum(case when dept_name = '{{ dept_nm }}' then 1 else 0 end) as {{ dept_nm }}_flag
+
+{%- if not loop.last -%}
+,
+{% endif -%}
+
+{%- endfor %}
+from dept
+group by 1
+)
+select * from final
