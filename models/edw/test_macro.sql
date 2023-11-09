b83871a6d0d627dde88ee7f244596ed82227eb97
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-11-09 09:57:20 +0000

    Bring actual column

--- models/edw/test_macro.sql
+++ models/edw/test_macro.sql
@@ -2,6 +2,6 @@ select
 CC_CALL_CENTER_ID,
 CC_MANAGER ,
 CC_COUNTRY ,
---CC_TAX_PERCENTAGE * 100 as CC_TAX_PERCENTAGE
+CC_TAX_PERCENTAGE as CC_TAX_PERCENTAGE_Actual
 {{ cc_percentage_cal('CC_TAX_PERCENTAGE') }} as CC_TAX_PERCENTAGE
 from {{ source('src', 'call_center_test') }}
