a38861a0165c25943a697c38121acead0048a98f
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-11-09 10:00:31 +0000

    Create grand_ops.sql under macros and execute code

---
+++ macros/grand_ops.sql
@@ -0,0 +1,13 @@
+{% macro grant_ops(schema=target.schema, role=target.role) %}
+
+{% set custom_sql %}
+grant usage on schema {{ schema }} to role {{ role }};
+grant select on all tables in schema {{ schema }} to role {{ role }};
+grant select on all views in schema {{ schema }} to role {{ role }};
+{% endset %}
+
+{{ log('providing grants on tables and views in schema ' ~ schema ~ 'to role ' ~ role, info = True)}}
+{% do run_query(custom_sql)%}
+{{ log('Grants provided', info = True)}}
+
+{% endmacro %}
