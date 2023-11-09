28e5799367652a28483faebc3a1cd94e1648d921
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-11-09 09:58:12 +0000

    Fix syntax error.

--- models/edw/test_macro.sql
+++ models/edw/test_macro.sql
@@ -2,6 +2,6 @@ select
 CC_CALL_CENTER_ID,
 CC_MANAGER ,
 CC_COUNTRY ,
-CC_TAX_PERCENTAGE as CC_TAX_PERCENTAGE_Actual
+CC_TAX_PERCENTAGE as CC_TAX_PERCENTAGE_Actual,
 {{ cc_percentage_cal('CC_TAX_PERCENTAGE') }} as CC_TAX_PERCENTAGE
 from {{ source('src', 'call_center_test') }}
