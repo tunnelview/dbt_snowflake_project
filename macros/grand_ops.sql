c8a91f83e128a6fe3e2de862c62d310c8c23af39
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-11-10 10:29:14 +0000

    Commit final code

--- macros/grand_ops.sql
+++ macros/grand_ops.sql
@@ -1,6 +1,7 @@
 {% macro grant_ops(schema=target.schema, role=target.role) %}
 
 {% set custom_sql %}
+use role sysadmin;
 grant usage on schema {{ schema }} to role {{ role }};
 grant select on all tables in schema {{ schema }} to role {{ role }};
 grant select on all views in schema {{ schema }} to role {{ role }};
