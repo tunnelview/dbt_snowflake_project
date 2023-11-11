617c31c259b9ddcf8cd6cde140e8cc6ac36e4224
Shibin Abraham <shibin.abraham1986@gmail.com>
2023-11-11 05:49:30 +0000

    Add test commit for repo. For the earlier message was "Pull from main".

--- macros/grand_ops.sql
+++ macros/grand_ops.sql
@@ -7,6 +7,7 @@ grant select on all tables in schema {{ schema }} to role {{ role }};
 grant select on all views in schema {{ schema }} to role {{ role }};
 {% endset %}
 
+
 {{ log('providing grants on tables and views in schema ' ~ schema ~ 'to role ' ~ role, info = True)}}
 {% do run_query(custom_sql)%}
 {{ log('Grants provided', info = True)}}
